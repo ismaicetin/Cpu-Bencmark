c42a6a8f55e61ae566f06954e192418f68c660c0
ismaicetin <ismaicetin@hotmail.com>
2019-10-01 17:59:29 +0300

    bitirme

--- App copy.js
+++ App copy.js
@@ -5,52 +5,41 @@ import { Block, Card, Text, Button, Input } from './galio-framework';
 import theme from './theme';
 import DATA from './data.json';
 
-import {YellowBox} from 'react-native';
-YellowBox.ignoreWarnings(['Require cycle']);
+import { ActivityIndicator  } from 'react-native'
 
- //console.disableYellowBox = true;
- //console.ignoredYellowBox = ['Require cycle'];
-
-
- DATA.sort(function (a, b) {
-  return a.rank - b.rank;
-});
 
 const { width } = Dimensions.get('screen');
 
- 
+function Item({ ngmodel }) {
+  return (
+    <Block style={styles.item}>
+      <Text style={styles.title}>{ngmodel.name}</Text>
+      <Text style={styles.title}>{ngmodel.rank}</Text>
+    </Block>
+  );
+}
 
 function handleChange(value) {
   console.log(value);
 };
 
 
-
+var filterData = DATA.filter(function (item, index) {
+  return index < 20;
+});
 
 
 export default class App extends Component {
    constructor(props) {
     super(props)
     this.state = {
-      data:[]
+      data: filterData || []
     }
- 
-
-  }
-
-  componentWillMount () {
-    this.fetchData(1);
-  }
-
-  fetchData = (page) => {
-    var filterData = DATA.filter(function (item, index) {
-      return index < 20;
-    });
     this.setState({ "data": filterData })
-    console.log(filterData)
-    return filterData
-  };
+    console.log(this.state.data);
 
+  }
+  
   renderSeparator = () => {
     return (
       <View
@@ -64,7 +53,7 @@ export default class App extends Component {
   };
   renderFooter = () => {
     //it will show indicator at the bottom of the list when data is loading otherwise it returns null
-     if (!this.state.loading) return null;
+    // if (!this.state.loading) return null;
      return (
        <ActivityIndicator
          style={{ color: '#000' }}
@@ -74,59 +63,35 @@ export default class App extends Component {
    handleLoadMore = () => {
     console.log("end");
     if (!this.state.loading) {
+      this.page = this.page + 1; // increase page by 1
      
     }
-
   };
  
   render() {
-   
     return (
-      <View style={styles.container}>
-        <Input
-    rounded
-    placeholder="Search"
-    style={{ width: width * 0.9 }}
-    onChangeText={text => handleChange(text)}
-  />
+       
        
         <FlatList
-        style={styles.flatList}
+        contentContainerStyle={{
+          flex: 1,
+          flexDirection: 'column',
+          height: '100%',
+          width: '100%'
+        }}
           data={this.state.data}
-          keyExtractor={ ( item, index )=> index.toString()}
-          
-          renderItem={({ item, index }) => (
-               
-              <Block >
-              <Text style={{textAlign:'left',padding:8}}>
-                  {item.rank}
-                  {'.'}
-                  {item.name.toUpperCase()}
-                </Text>
-              </Block>
-
-            )}
+          keyExtractor={item => item.id}
+          renderItem={({ item, index }) => {
+            return (
+              <Item ngmodel={item} key={index} />
+            );
+          }}
           ItemSeparatorComponent={this.renderSeparator}
           ListFooterComponent={this.renderFooter}
           onEndReachedThreshold={1}
-          onEndReached={() => this.handleLoadMore()}   
+          onEndReached={alert("son")}
         />
-
-       { /*<Card
-
-          borderless
-          style={styles.card}
-          title="Christopher Moon"
-          caption="139 minutes ago"
-          location="Los Angeles, CA"
-          avatar="http://i.pravatar.cc/100?id=skater"
-          imageStyle={styles.cardImageRadius}
-          imageBlockStyle={{ padding: theme.SIZES.BASE / 2 }}
-          image="https://images.unsplash.com/photo-1497802176320-541c8e8de98d?&w=1600&h=900&fit=crop&crop=entropy&q=300"
-       />*/}
-
-
-</View>
+ 
     );
   }
 }
@@ -134,15 +99,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:theme.COLORS.MUTED,
-  
-  },
-  flatList:{
-    flex:1,
-    backgroundColor:theme.COLORS.WHITE,
-    
-   },
-   cards: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }, cards: {
     width,
     backgroundColor: theme.COLORS.WHITE,
     alignItems: 'center',
